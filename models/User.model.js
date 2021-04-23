a8e4419fba89e4b3f5edbc37cfeaa20c543c9689
Joshua George <horheyinc8@gmail.com>
2021-04-23 22:41:07 +0200

    trying to pull from master

--- models/User.model.js
+++ models/User.model.js
@@ -14,32 +14,3 @@ const userSchema = new Schema({
 const User = model("User", userSchema);
 
 module.exports = User;
-
-// second collection of all the trip data
-const tripSchema = new Schema({
-  destination: {
-    type: String,
-    required: true,
-  },
-
-  budget: { type: Number, required: true },
-
-  timeuntil: {
-    type: Number,
-    required: true,
-  },
-
-  length: {
-    type: Number,
-    required: true,
-  },
-
-  luxury: {
-    type: String,
-    required: true,
-  },
-});
-
-const Trip = model("trip", tripSchema);
-
-module.exports = Trip;
--- routes/index.js
+++ routes/index.js
@@ -7,64 +7,64 @@ router.get("/", (req, res, next) => {
 
 //get route for login page to view your planned vacations
 router.get("/yourtrips", (req, res) => {
-  res.render("lab/yourtrips.hbs");
+  res.render("trips/yourtrips.hbs");
 });
 
-//after signUP page, this route goes to create a trip page
+//after signUP page, this route goes to create a trips page
 router.get("/create", (req, res) => {
-  res.render("lab/create.hbs");
+  res.render("trips/create.hbs");
 });
 
 //Get route to show destination page after create page
 router.get("/destination", (req, res) => {
-  res.render("lab/destination.hbs");
+  res.render("trips/destination.hbs");
 });
-//POST route for your trips destination
-router.post("/destination/:id", (req, res, next) => {
-  //get the id
-  const { id } = req.params;
+// //POST route for your tripss destination
+// router.post("/destination/:id", (req, res, next) => {
+//   //get the id
+//   const { id } = req.params;
 
-  //get body with the elements to be edited
-  const { username, password } = req.body;
-  //go to the DB and edit the element
-  Trip.findByIdAndUpdate(id, { destination })
-    .then((data) => {
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
+//   //get body with the elements to be edited
+//   const { username, password } = req.body;
+//   //go to the DB and edit the element
+//   trips.findByIdAndUpdate(id, { destination })
+//     .then((data) => {
+//       res.redirect("/");
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 //Get route to show budget page after destinations page
 router.get("/budget", (req, res) => {
-  res.render("lab/budget.hbs");
+  res.render("trips/budget.hbs");
 });
 
 //Get route to show timeuntil page after destination page
-// this page will ask the user when do they want to take the trip
+// this page will ask the user when do they want to take the trips
 router.get("/timeuntil", (req, res) => {
-  res.render("lab/timeuntil.hbs");
+  res.render("trips/timeuntil.hbs");
 });
 
 //Get route to show length of the vaction (1wk, 2wks. 3wks) after timeuntil page
 router.get("/length", (req, res) => {
-  res.render("lab/length.hbs");
+  res.render("trips/length.hbs");
 });
 
 //Get route to show luxury page after length page
 //will ask the user the level of luxury they want
 router.get("/luxury", (req, res) => {
-  res.render("lab/luxury.hbs");
+  res.render("trips/luxury.hbs");
 });
 
 //Get route to show total page after luxury page
-//this page will show the user the est total cost of the trip
+//this page will show the user the est total cost of the trips
 router.get("/total", (req, res) => {
-  res.render("lab/total.hbs");
+  res.render("trips/total.hbs");
 });
 
 //Get route to show pichart page after total page, breaking down the expensies
 router.get("/piechart", (req, res) => {
-  res.render("lab/piechart.hbs");
+  res.render("trips/piechart.hbs");
 });
 
 module.exports = router;
