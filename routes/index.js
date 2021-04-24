76a6b988dbf01643a69272024b8c5cf28c163c4d
Joshua George <horheyinc8@gmail.com>
2021-04-24 10:15:22 +0200

    some pages in hbs outlined

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,167 @@
 const router = require("express").Router();
 
-/* GET home page */
+/* GET landing page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get("/home", (req, res) => {
-  res.render("auth/home.hbs");
+//get route for home (profile) page
+//*********Create a trip Route*********
+//GET route
+
+// //after signUP page, this route goes to create a trips page
+// router.get("/create", (req, res) => {
+//   res.render("trips/create.hbs");
+// });
+router.get("/home/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  Trip.findById(id)
+    .then((data) => {
+      res.render("create.hbs", { data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-//get route for login page to view your planned vacations
-router.get("/yourtrips", (req, res) => {
-  res.render("trips/yourtrips.hbs");
+//****Create a trip page***** */
+//POST route for *create* page
+router.post("/home/:id/create", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
+
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { name })
+    .then((data) => {
+      res.redirect("/destination");
+    })
+    .catch((err) => console.log(err));
 });
 
-//after signUP page, this route goes to create a trips page
-router.get("/create", (req, res) => {
-  res.render("trips/create.hbs");
-});
-
+//********Destination Page***** */
 //Get route to show destination page after create page
 router.get("/destination", (req, res) => {
-  res.render("trips/destination.hbs");
+  res.render("destination.hbs");
 });
-// //POST route for your tripss destination
-// router.post("/destination/:id", (req, res, next) => {
-//   //get the id
-//   const { id } = req.params;
 
-//   //get body with the elements to be edited
-//   const { username, password } = req.body;
-//   //go to the DB and edit the element
-//   trips.findByIdAndUpdate(id, { destination })
-//     .then((data) => {
-//       res.redirect("/");
-//     })
-//     .catch((err) => console.log(err));
-// });
+//POST route for destination page
+router.post("/destination/:id", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
 
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { destination })
+    .then((data) => {
+      res.redirect("/budget");
+    })
+    .catch((err) => console.log(err));
+});
+
+//******budget page */
 //Get route to show budget page after destinations page
 router.get("/budget", (req, res) => {
-  res.render("trips/budget.hbs");
+  res.render("budget.hbs");
 });
+//POST route for budget page
+router.post("/budget/:id", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
 
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { budget })
+    .then((data) => {
+      res.redirect("/timeuntil");
+    })
+    .catch((err) => console.log(err));
+});
+git a
+//*****time until the user takes their trip page  */
 //Get route to show timeuntil page after destination page
 // this page will ask the user when do they want to take the trips
 router.get("/timeuntil", (req, res) => {
-  res.render("trips/timeuntil.hbs");
+  res.render("timeuntil.hbs");
+});
+//POST route for timeuntil page
+router.post("/timeuntil/:id", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
+
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { approxDate })
+    .then((data) => {
+      res.redirect("/length");
+    })
+    .catch((err) => console.log(err));
 });
 
+//******Length of trip page ********/
 //Get route to show length of the vaction (1wk, 2wks. 3wks) after timeuntil page
 router.get("/length", (req, res) => {
-  res.render("trips/length.hbs");
+  res.render("length.hbs");
+});
+//POST route for length page
+router.post("/length/:id", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
+
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { lengthInWeeks })
+    .then((data) => {
+      res.redirect("/luxury");
+    })
+    .catch((err) => console.log(err));
 });
 
-//Get route to show luxury page after length page
+//Get route to show *******luxury****** page after length page
 //will ask the user the level of luxury they want
 router.get("/luxury", (req, res) => {
-  res.render("trips/luxury.hbs");
+  res.render("luxury.hbs");
+});
+//POST route for luxury page
+router.post("/luxury/:id", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
+
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { luxury })
+    .then((data) => {
+      res.redirect("/total");
+    })
+    .catch((err) => console.log(err));
 });
 
-//Get route to show total page after luxury page
+//Get route to show ****** total ****** page after luxury page
 //this page will show the user the est total cost of the trips
 router.get("/total", (req, res) => {
-  res.render("trips/total.hbs");
+  res.render("total.hbs");
+});
+//POST route for total page
+router.post("/total/:id", (req, res, next) => {
+  //get the id
+  const { id } = req.params;
+
+  //get body with the elements to be edited
+  const { username, password } = req.body;
+  //go to the DB and edit the element
+  Trip.findByIdAndUpdate(id, { total })
+    .then((data) => {
+      res.redirect("/piechart");
+    })
+    .catch((err) => console.log(err));
 });
 
 //Get route to show pichart page after total page, breaking down the expensies
